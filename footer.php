5166eaba9fc5336f9c06c34fc7ab2f85caf3be0d
TranDuy148 <namleo1408@gmail.com>
2023-03-29 23:12:12 +0700

    add project

---
+++ footer.php
@@ -0,0 +1,57 @@
+<?php 
+    
+    ?>
+    <!--::footer_part start::-->
+    <footer class="footer_part" style=" margin-top: 10px; padding: 20px; background-color: #F8F8F8; background-clip: padding;">
+        <div class="container">
+            <div class="row justify-content-around">
+                <div class="col-sm-6 col-lg-3">
+                    <div class="single_footer_part">
+                        <h4>Thông Tin Cửa Hàng</h4>
+                        <ul class="list-unstyled">
+                            <li><a href="">Cửa Hàng Zmobile</a></li>
+                            <li><a href="">33 Lương Thế Vinh, P. Văn Quán, Thanh Xuân, Hà Nội</a></li>
+                            <li><a href="">Hotline: 0395.192.933</a></li>
+                        </ul>
+                    </div>
+                </div>
+                
+                <div class="col-sm-6 col-lg-3">
+                    <div class="single_footer_part">
+                        <h4>Chính Sách</h4>
+                        <ul class="list-unstyled">
+                            <li><a href="">Quy chế hoạt động</a></li>
+                            <li><a href="">Chính sách Bảo hành</a></li>
+                            <li><a href="">Liên hệ hợp tác kinh doanh</a></li>
+                            <li><a href="">Đơn Doanh nghiệp</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-lg-3">
+                    <div class="single_footer_part">
+                        <h4>Bảo Hành</h4>
+                        <ul class="list-unstyled">
+                            <li><a href="">Tra thông tin bảo hành</a></li>
+                            <li><a href="">Trung tâm bảo hành chính hãng</a></li>
+                            <li><a href="">Quy định về việc sao lưu dữ liệu</a></li>
+                            <li><a href="">Dịch vụ bảo hành điện thoại</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-lg-3">
+                    <div class="single_footer_part">
+                        <h4>Hotline</h4>
+                        <ul class="list-unstyled">
+                            <li><a href="">Gọi mua hàng: 0395.192.933</a></li>
+                            <li><a href="">Gọi kiếu nại: 0395.192.933</a></li>
+                            <li><a href="">Gọi bảo hành: 0395.192.933</a></li>
+                            <li><a href="">(8h00 - 22h00)</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+        
+    </footer>
+    <!--::footer_part end::-->
